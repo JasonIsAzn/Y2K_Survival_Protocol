202d4d61aced96a39db31fa7b1592155d5c9e586
Jason <lejason123@gmail.com>
2024-10-12 23:17:39 -0500

    added top bar

--- app/page.tsx
+++ app/page.tsx
@@ -14,19 +14,27 @@ export default function Home() {
 
       <main className="flex justify-center items-center h-full w-full">
         {/* Chat Box */}
-        <div className="h-[70%] w-[50%] bg-white border-blue-700 border-[10px] rounded-lg">
-          <div className="bg-blue-700 flex justify-end">
-            <div className="w-10 h-10 bg-blue-500 border rounded-lg"></div>
-            <div className="w-10 h-10 bg-blue-500 border rounded-lg"></div>
-            <div className="w-10 h-10 bg-red-500 border rounded-lg flex items-center justify-center">
-              <span className=" text-white text-3xl bg-blue-700 leading-none">X</span>
+        <div className="h-[70%] w-[40%] bg-white border-blue-700 border-[10px] rounded-lg">
+          <div className="">
+            <div className="bg-blue-700 flex justify-end">
+              <div className="w-10 h-10 bg-blue-500 border rounded-lg">
+                <div className="text-white text-2xl ml-2">_</div>
+              </div>
+              <div className="w-10 h-10 bg-blue-500 border rounded-lg flex items-center justify-center">
+                <div className="w-5 h-5 bg-blue-500 border-white border-[2px]">
+                  <div className="w-4 h-1 bg-white"></div>
+                </div>
+
+              </div>
+              <div className="w-10 h-10 bg-red-500 border rounded-lg flex items-center justify-center">
+                <div className="text-white text-2xl ml-1">X</div>
+              </div>
             </div>
+
+            <div className="text-black text-3xl text-center mt-14">Y2K: Survival Protocol</div>
+            {/** Start button **/}
+            <div className="text-blue-950 text-2xl text-center mt-44">Start</div>
           </div>
-
-          <div className="text-black text-3xl text-center mt-2">Y2K: Survival Protocol</div>
-          {/** Start button **/}
-          <div className="text-blue-950 text-2xl text-center mt-20">Start</div>
-
 
 
         </div>
